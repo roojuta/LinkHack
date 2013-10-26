404abf7348079d764f30ad1f3770909b61df4285
Swetha Karthik <skarthik@linkedin.com>
2013-10-26 04:54:23 -0700

    Adding webapp changes

--- recogenie/src/main/java/com/linkedin/hack/controller/ReccomendAndMessageController.java
+++ recogenie/src/main/java/com/linkedin/hack/controller/ReccomendAndMessageController.java
@@ -2,6 +2,7 @@ package com.linkedin.hack.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,9 +14,11 @@ public class ReccomendAndMessageController
   {
     // TODO Auto-generated constructor stub
   }
-  
+
   @RequestMapping("/index")
-  public @ResponseBody ModelAndView getIndex(){
+  public @ResponseBody
+  ModelAndView getIndex()
+  {
     ModelAndView mv = new ModelAndView("index");
     mv.addObject("name", "Swetha");
     return mv;
