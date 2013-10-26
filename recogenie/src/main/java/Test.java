d3b0cb4c344c78437812c807ac2c0affa8102f61
Swetha Karthik <skarthik@linkedin.com>
2013-10-26 02:41:53 -0700

    Adding web app

---
+++ recogenie/src/main/java/Test.java
@@ -0,0 +1,19 @@
+
+public class Test
+{
+
+  public Test()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+   System.out.println("hello!");
+
+  }
+
+}
